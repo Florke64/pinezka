2c262fc83c502a068020a5735e7b1b8cb2624375
Daniel Wasiak <kontakt@flrque.win>
2022-03-17 18:29:08 +0100

    Added Circle in Rectangle target rendering

--- app.py
+++ app.py
@@ -1,13 +1,49 @@
 #!/usr/bin/env python3
 import sys
+import math
 import pygame
 
-SCALE = 2
-W_WIDTH = 800
-W_HEIGHT = 600
+SCALE = 1.314
+W_WIDTH = 1280
+W_HEIGHT = 720
 FPS_COUNT = 24
-WINDOW_TITLE = "PiNezka by Wasiak Florke Daniel <wasiak.daniel@gmail.com>"
+WINDOW_TITLE = "PiNezka | Author: Wasiak Florke Daniel <wasiak.daniel@gmail.com>"
 WINDOW_RESOLUTION = (W_WIDTH, W_HEIGHT)
+
+TARGET_CENTER = (W_WIDTH/2, W_HEIGHT/2)
+TARGET_SIZE = 400
+
+
+class Target:
+    def __init__(self, center):
+        self._center = center
+        self._point = center[0]-TARGET_SIZE/2, center[1]-TARGET_SIZE/2
+        self._width = TARGET_SIZE
+
+    def draw(self, window):
+        line_color = (200, 200, 200)
+
+        pygame.draw.rect(window, line_color, (self._point[0], self._point[1], self._width, self._width), 2)
+        pygame.draw.circle(window, line_color, self._center, self._width/2, 2)
+
+    def is_in_rect(self, point):
+        return (self._point[0] < point[0] < self._point[0] + self._width) and \
+               (self._point[1] < point[1] < self._point[1] + self._width)
+
+    def is_in_circle(self, point):
+        if not self.is_in_rect(point):
+            return False
+
+        a = math.pow(self._center[0] - point[0], 2)
+        b = math.pow(self._center[1] - point[1], 2)
+        if math.sqrt(a + b) < self._width/2:
+            return True
+
+        return False
+
+
+global TARGET
+TARGET = Target(TARGET_CENTER)
 
 
 def draw_checker(window):
@@ -19,11 +55,11 @@ def draw_checker(window):
 
     for i in range(int(vertical_lines)):
         x = i*spacing
-        pygame.draw.line(window, line_color, (x, 0), (x, 600))
+        pygame.draw.line(window, line_color, (x, 0), (x, W_HEIGHT))
 
     for i in range(int(horizontal_lines)):
         y = i*spacing
-        pygame.draw.line(window, line_color, (0, y), (800, y))
+        pygame.draw.line(window, line_color, (0, y), (W_WIDTH, y))
 
 
 class PiNezka:
@@ -49,12 +85,19 @@ class PiNezka:
         # cosmetic aspect of the background
         draw_checker(self._window)
 
+        global TARGET
+        TARGET.draw(self._window)
+
         pygame.display.update()
 
     def _pygame_event_handler(self) -> None:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self._running = False
+
+            if event.type == pygame.MOUSEBUTTONUP:
+                pos = pygame.mouse.get_pos()
+                print(TARGET.is_in_circle(pos))
 
     def mainloop(self):
         while self._running:
