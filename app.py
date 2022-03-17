f1d37e3f1eb323e1585400fea3eb84a55e97065a
Daniel Wasiak <kontakt@flrque.win>
2022-03-17 17:49:31 +0100

    Added checker background

--- app.py
+++ app.py
@@ -2,15 +2,28 @@
 import sys
 import pygame
 
+SCALE = 2
 W_WIDTH = 800
 W_HEIGHT = 600
 FPS_COUNT = 24
 WINDOW_TITLE = "PiNezka by Wasiak Florke Daniel <wasiak.daniel@gmail.com>"
 WINDOW_RESOLUTION = (W_WIDTH, W_HEIGHT)
 
-def draw_line(window):
-    pygame.draw.line(window, (0, 0, 0), (60, 80), (130, 100))
-    pass
+
+def draw_checker(window):
+    line_color = (30, 30, 30)
+
+    spacing = (10 * SCALE)
+    vertical_lines = W_WIDTH / spacing      # 800 / 10 = 80
+    horizontal_lines = W_HEIGHT / spacing
+
+    for i in range(int(vertical_lines)):
+        x = i*spacing
+        pygame.draw.line(window, line_color, (x, 0), (x, 600))
+
+    for i in range(int(horizontal_lines)):
+        y = i*spacing
+        pygame.draw.line(window, line_color, (0, y), (800, y))
 
 
 class PiNezka:
@@ -31,9 +44,10 @@ class PiNezka:
         self._pygame_event_handler()
 
     def draw(self):
-        self._window.fill((168, 200, 192))
+        self._window.fill((0, 0, 0))
 
-        draw_line(self._window)
+        # cosmetic aspect of the background
+        draw_checker(self._window)
 
         pygame.display.update()
 
